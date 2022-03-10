64b660c76e64b0f40ffa070f41a7efd10473dd56
zhangyunhe <zhangyunhe1114@gmail.com>
2022-03-10 11:26:46 +0800

    add copy nft ut

--- tests/synft.ts
+++ tests/synft.ts
@@ -735,70 +735,70 @@ describe("synft", () => {
     }
   });
 
-  // it("copy nft", async () => {
-  //   let connection = anchor.getProvider().connection;
-  //   let name = "copy_nft";
-  //   let symbol = "nft_symbol";
-  //   let uri = "https://arweave.net/MwkMActRVmKND2t3Bq1qzrT7PdWtO-ZPVnZxh5SQooA";
+  it("copy nft", async () => {
+    let connection = anchor.getProvider().connection;
+    let name = "copy_nft";
+    let symbol = "nft_symbol";
+    let uri = "https://arweave.net/MwkMActRVmKND2t3Bq1qzrT7PdWtO-ZPVnZxh5SQooA";
 
-  //   const [_nft_mint_pda, _nft_mint_bump] = await PublicKey.findProgramAddress(
-  //     [
-  //       Buffer.from(anchor.utils.bytes.utf8.encode("synthetic-nft-mint-seed")),
-  //       mint5.toBuffer(),
-  //     ],
-  //     program.programId
-  //   );
-  //   console.log("_nft_mint_pda:", _nft_mint_pda);
+    const [_nft_mint_pda, _nft_mint_bump] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from(anchor.utils.bytes.utf8.encode("synthetic-nft-mint-seed")),
+        mint5.toBuffer(),
+      ],
+      program.programId
+    );
+    console.log("_nft_mint_pda:", _nft_mint_pda);
 
-  //   const [_nft_token_account_pda, _nft_token_account_bump] =
-  //     await PublicKey.findProgramAddress(
-  //       [
-  //         Buffer.from(
-  //           anchor.utils.bytes.utf8.encode("synthetic-nft-account-seed")
-  //         ),
-  //         user5.publicKey.toBuffer(),
-  //       ],
-  //       program.programId
-  //     );
+    const [_nft_token_account_pda, _nft_token_account_bump] =
+      await PublicKey.findProgramAddress(
+        [
+          Buffer.from(
+            anchor.utils.bytes.utf8.encode("synthetic-nft-account-seed")
+          ),
+          user5.publicKey.toBuffer(),
+        ],
+        program.programId
+      );
 
-  //   const metadataProgramId = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";
-  //   const [_nft_metadata_pda, _nft_metadata_bump] =
-  //     await PublicKey.findProgramAddress(
-  //       [
-  //         Buffer.from(anchor.utils.bytes.utf8.encode("metadata")),
-  //         new PublicKey(metadataProgramId).toBuffer(),
-  //         _nft_mint_pda.toBuffer(),
-  //       ],
-  //       new PublicKey(metadataProgramId)
-  //     );
-  //   console.log("_nft_metadata_pda: ", _nft_metadata_pda);
+    const metadataProgramId = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";
+    const [_nft_metadata_pda, _nft_metadata_bump] =
+      await PublicKey.findProgramAddress(
+        [
+          Buffer.from(anchor.utils.bytes.utf8.encode("metadata")),
+          new PublicKey(metadataProgramId).toBuffer(),
+          _nft_mint_pda.toBuffer(),
+        ],
+        new PublicKey(metadataProgramId)
+      );
+    console.log("_nft_metadata_pda: ", _nft_metadata_pda);
 
-  //   let nftCopyTx = await program.rpc.nftCopy(name, symbol, uri, {
-  //     accounts: {
-  //       currentOwner: user5.publicKey,
-  //       fromNftMint: mint5,
-  //       nftMetaDataAccount: _nft_metadata_pda,
-  //       nftMintAccount: _nft_mint_pda,
-  //       nftTokenAccount: _nft_token_account_pda,
+    let nftCopyTx = await program.rpc.nftCopy(name, symbol, uri, {
+      accounts: {
+        currentOwner: user5.publicKey,
+        fromNftMint: mint5,
+        nftMetaDataAccount: _nft_metadata_pda,
+        nftMintAccount: _nft_mint_pda,
+        nftTokenAccount: _nft_token_account_pda,
 
-  //       systemProgram: anchor.web3.SystemProgram.programId,
-  //       rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-  //       tokenProgram: TOKEN_PROGRAM_ID,
-  //       mplProgram: MPL_PROGRAM_ID,
-  //     },
-  //     signers: [user5],
-  //   });
-  //   console.log("nftCopyTx: ", nftCopyTx);
+        systemProgram: anchor.web3.SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        mplProgram: MPL_PROGRAM_ID,
+      },
+      signers: [user5],
+    });
+    console.log("nftCopyTx: ", nftCopyTx);
 
-  //   // validate _nft_token_account_pda
-  //   let nftTokenAccount = await getAccount(connection, _nft_token_account_pda);
-  //   assert.ok(nftTokenAccount.mint.toString() == _nft_mint_pda.toString());
+    // validate _nft_token_account_pda
+    let nftTokenAccount = await getAccount(connection, _nft_token_account_pda);
+    assert.ok(nftTokenAccount.mint.toString() == _nft_mint_pda.toString());
 
-  //   let nftMetadata = await getNFTMetadata(
-  //     _nft_mint_pda.toBase58(),
-  //     connection,
-  //     _nft_metadata_pda.toBase58()
-  //   );
-  //   console.log("nftMetadata: ", nftMetadata);
-  // });
+    let nftMetadata = await getNFTMetadata(
+      _nft_mint_pda.toBase58(),
+      connection,
+      _nft_metadata_pda.toBase58()
+    );
+    console.log("nftMetadata: ", nftMetadata);
+  });
 });
