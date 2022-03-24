4f6565326a54e409a2530c92f65749e04eca0a3a
zhangyunhe <zhangyunhe1114@gmail.com>
2022-03-24 11:49:23 +0800

    refactor transfer crank]

--- programs/synft/src/instructions/transfer_crank_v2.rs
+++ programs/synft/src/instructions/transfer_crank_v2.rs
@@ -96,7 +96,39 @@ pub fn handler(ctx: Context<TransferCrank>) -> Result<()> {
         }
     }
     if !is_crank_meta_inited {
-        return init(ctx);
+        ctx.accounts.children_meta.parent = *ctx.accounts.parent_mint_account.to_account_info().key;
+        ctx.accounts.children_meta.root = *ctx.accounts.children_meta.to_account_info().key;
+        ctx.accounts.children_meta.is_mutated = false;
+        ctx.accounts.crank_meta.old_root_meta_data = *ctx.accounts.root_meta.to_account_info().key;
+        ctx.accounts.crank_meta.new_root_meta_data =
+            *ctx.accounts.children_meta.to_account_info().key;
+        for immediate_child in ctx.accounts.parent_meta.immediate_children.iter_mut() {
+            if immediate_child.to_bytes() != Pubkey::default().to_bytes() {
+                for not_processed_child in ctx.accounts.crank_meta.not_processed_children.iter_mut()
+                {
+                    if not_processed_child.to_bytes() == Pubkey::default().to_bytes() {
+                        *not_processed_child = immediate_child.clone();
+                    }
+                }
+            }
+        }
+        ctx.accounts.parent_meta.height = 1;
+        ctx.accounts
+            .children_meta_of_parent
+            .close(ctx.accounts.operator.to_account_info())?;
+        for immediate_child in ctx
+            .accounts
+            .parent_meta_of_parent
+            .immediate_children
+            .iter_mut()
+        {
+            if immediate_child.to_bytes()
+                != (*ctx.accounts.child_mint_account.to_account_info().key).to_bytes()
+            {
+                *immediate_child = Pubkey::default().clone();
+            }
+        }
+        return Ok(());
     }
 
     //2. process
@@ -119,7 +151,6 @@ pub fn handler(ctx: Context<TransferCrank>) -> Result<()> {
     //2.1 process leaf or non-leaf
     let mut is_leaf: bool = true;
     for immediate_child in ctx.accounts.parent_meta.immediate_children.iter() {
-        // TODO need to verify by default public key
         if immediate_child.to_bytes() != Pubkey::default().to_bytes() {
             is_leaf = false;
             break;
@@ -159,38 +190,4 @@ pub fn handler(ctx: Context<TransferCrank>) -> Result<()> {
             .close(ctx.accounts.operator.to_account_info())?;
     }
     Ok(())
-}
-
-fn init(ctx: Context<TransferCrank>) -> Result<()> {
-    ctx.accounts.children_meta.parent = *ctx.accounts.parent_mint_account.to_account_info().key;
-    ctx.accounts.children_meta.root = *ctx.accounts.children_meta.to_account_info().key;
-    ctx.accounts.children_meta.is_mutated = false;
-    ctx.accounts.crank_meta.old_root_meta_data = *ctx.accounts.root_meta.to_account_info().key;
-    ctx.accounts.crank_meta.new_root_meta_data = *ctx.accounts.children_meta.to_account_info().key;
-    for immediate_child in ctx.accounts.parent_meta.immediate_children.iter_mut() {
-        if immediate_child.to_bytes() != Pubkey::default().to_bytes() {
-            for not_processed_child in ctx.accounts.crank_meta.not_processed_children.iter_mut() {
-                if not_processed_child.to_bytes() == Pubkey::default().to_bytes() {
-                    *not_processed_child = immediate_child.clone();
-                }
-            }
-        }
-    }
-    ctx.accounts.parent_meta.height = 1;
-    ctx.accounts
-        .children_meta_of_parent
-        .close(ctx.accounts.operator.to_account_info())?;
-    for immediate_child in ctx
-        .accounts
-        .parent_meta_of_parent
-        .immediate_children
-        .iter_mut()
-    {
-        if immediate_child.to_bytes()
-            != (*ctx.accounts.child_mint_account.to_account_info().key).to_bytes()
-        {
-            *immediate_child = Pubkey::default().clone();
-        }
-    }
-    return Ok(());
 }
