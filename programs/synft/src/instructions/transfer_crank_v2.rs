a8b97d2557983e57094bbf7c389c1260e589c396
zhangyunhe <zhangyunhe1114@gmail.com>
2022-03-24 10:36:50 +0800

    modify transfer func

--- programs/synft/src/instructions/transfer_crank_v2.rs
+++ programs/synft/src/instructions/transfer_crank_v2.rs
@@ -87,8 +87,7 @@ pub struct TransferCrank<'info> {
 }
 
 pub fn handler(ctx: Context<TransferCrank>) -> Result<()> {
-    // TODO
-    //1.init 判断是否是crank_meta init
+    //1.init
     let mut is_crank_meta_inited: bool = false;
     for child in ctx.accounts.parent_meta.immediate_children.iter_mut() {
         if child.to_bytes() != PLACEHOLDER_PUBKEY.to_bytes() {
@@ -97,34 +96,10 @@ pub fn handler(ctx: Context<TransferCrank>) -> Result<()> {
         }
     }
     if !is_crank_meta_inited {
-        // a 设置children_meta的root、parent
-        ctx.accounts.children_meta.parent = *ctx.accounts.parent_mint_account.to_account_info().key;
-        ctx.accounts.children_meta.root = *ctx.accounts.children_meta.to_account_info().key;
-        ctx.accounts.children_meta.is_mutated = false;
-        // b 设置crank_meta 属性数据
-        ctx.accounts.crank_meta.old_root_meta_data = *ctx.accounts.root_meta.to_account_info().key;
-        ctx.accounts.crank_meta.new_root_meta_data = *ctx.accounts.children_meta.to_account_info().key;
-        ctx.accounts.crank_meta.not_processed_children = [PLACEHOLDER_PUBKEY; 32];
-        for immediate_child in ctx.accounts.parent_meta.immediate_children.iter_mut() {
-            if immediate_child.to_bytes() != PLACEHOLDER_PUBKEY.to_bytes() {
-                for not_processed_child in ctx.accounts.crank_meta.not_processed_children.iter_mut()
-                {
-                    if not_processed_child.to_bytes() == PLACEHOLDER_PUBKEY.to_bytes() {
-                        *not_processed_child = immediate_child.clone();
-                    }
-                }
-            }
-        }
-        // c 层高设置为1
-        ctx.accounts.parent_meta.height = 1;
-        // d close children_meta_of_parent
-        ctx.accounts
-            .children_meta_of_parent
-            .close(ctx.accounts.operator.to_account_info())?;
-        return Ok(());
+        return init(ctx);
     }
 
-    //2. process 否则判断child_mint_account是否在crank_meta的not_processed_children里面
+    //2. process non-leaf
     let mut is_in_processed_children: bool = false;
     for not_processed_child in ctx.accounts.crank_meta.not_processed_children.iter_mut() {
         if not_processed_child.to_bytes()
@@ -137,13 +112,11 @@ pub fn handler(ctx: Context<TransferCrank>) -> Result<()> {
     if !is_in_processed_children {
         return err!(ErrorCode::InvalidTransferCrank);
     }
-    // a 设置children_meta的root
     ctx.accounts.children_meta.root = ctx.accounts.crank_meta.new_root_meta_data;
-    // b 层高设置上一层+1
     ctx.accounts.parent_meta.height = ctx.accounts.parent_meta_of_parent.height + 1;
     ctx.accounts.children_meta_of_parent.is_mutated = false;
     ctx.accounts.children_meta.is_mutated = true;
-    //3.判断是否是叶子节点（根据parent_meta是否为空判断）
+    //3. process leaf
     let mut is_leaf: bool = false;
     for immediate_child in ctx.accounts.parent_meta.immediate_children.iter_mut() {
         if immediate_child.to_bytes() == PLACEHOLDER_PUBKEY.to_bytes() {
@@ -159,7 +132,6 @@ pub fn handler(ctx: Context<TransferCrank>) -> Result<()> {
             .parent_meta
             .close(ctx.accounts.operator.to_account_info())?;
     } else {
-        // a 添加parent meta中的children数据到crank_meta的not_processed_children里面
         for immediate_child in ctx.accounts.parent_meta.immediate_children.iter_mut() {
             if immediate_child.to_bytes() != PLACEHOLDER_PUBKEY.to_bytes() {
                 for not_processed_child in ctx.accounts.crank_meta.not_processed_children.iter_mut()
@@ -171,7 +143,7 @@ pub fn handler(ctx: Context<TransferCrank>) -> Result<()> {
             }
         }
     }
-    //4.判断crank_meta的not_processed_children 是否为空
+    //4.end
     let mut is_processed_children_empty: bool = true;
     for not_processed_child in ctx.accounts.crank_meta.not_processed_children.iter_mut() {
         if not_processed_child.to_bytes() != PLACEHOLDER_PUBKEY.to_bytes() {
@@ -180,12 +152,45 @@ pub fn handler(ctx: Context<TransferCrank>) -> Result<()> {
         }
     }
     if is_processed_children_empty {
-        // a 修改rootmeta的is_mutated为false
         ctx.accounts.root_meta.is_mutated = false;
-        // b close crank_meta
         ctx.accounts
-        .crank_meta
-        .close(ctx.accounts.operator.to_account_info())?;
+            .crank_meta
+            .close(ctx.accounts.operator.to_account_info())?;
     }
     Ok(())
+}
+
+fn init(ctx: Context<TransferCrank>) -> Result<()> {
+    ctx.accounts.children_meta.parent = *ctx.accounts.parent_mint_account.to_account_info().key;
+    ctx.accounts.children_meta.root = *ctx.accounts.children_meta.to_account_info().key;
+    ctx.accounts.children_meta.is_mutated = false;
+    ctx.accounts.crank_meta.old_root_meta_data = *ctx.accounts.root_meta.to_account_info().key;
+    ctx.accounts.crank_meta.new_root_meta_data = *ctx.accounts.children_meta.to_account_info().key;
+    ctx.accounts.crank_meta.not_processed_children = [PLACEHOLDER_PUBKEY; 32];
+    for immediate_child in ctx.accounts.parent_meta.immediate_children.iter_mut() {
+        if immediate_child.to_bytes() != PLACEHOLDER_PUBKEY.to_bytes() {
+            for not_processed_child in ctx.accounts.crank_meta.not_processed_children.iter_mut() {
+                if not_processed_child.to_bytes() == PLACEHOLDER_PUBKEY.to_bytes() {
+                    *not_processed_child = immediate_child.clone();
+                }
+            }
+        }
+    }
+    ctx.accounts.parent_meta.height = 1;
+    ctx.accounts
+        .children_meta_of_parent
+        .close(ctx.accounts.operator.to_account_info())?;
+    for immediate_child in ctx
+        .accounts
+        .parent_meta_of_parent
+        .immediate_children
+        .iter_mut()
+    {
+        if immediate_child.to_bytes()
+            != (*ctx.accounts.child_mint_account.to_account_info().key).to_bytes()
+        {
+            *immediate_child = PLACEHOLDER_PUBKEY.clone();
+        }
+    }
+    return Ok(());
 }
