3de7581ecae37aa770e644188d119d68ae5881d2
zhangyunhe <zhangyunhe1114@gmail.com>
2022-03-29 14:21:45 +0800

    add render js demo

--- render_scripts/common.ts
+++ render_scripts/common.ts
@@ -1,9 +1,10 @@
 import * as anchor from "@project-serum/anchor";
 import { Synft } from "./synft";
+const PublicKey = anchor.web3.PublicKey;
+
 function initSynftProgram() {
   const idl = require("./idl.json");
   const web3 = anchor.web3;
-  const PublicKey = anchor.web3.PublicKey;
   const Keypair = anchor.web3.Keypair;
   const keypairId =
     "[125,80,6,196,91,20,194,252,89,75,162,187,65,171,144,51,78,30,111,57,16,210,215,237,249,62,74,70,114,65,243,219,32,151,221,17,102,219,199,185,61,37,45,203,89,189,58,81,92,110,55,99,13,91,180,110,213,80,138,102,156,39,81,244]";
@@ -11,7 +12,7 @@ function initSynftProgram() {
     Keypair.fromSecretKey(Buffer.from(JSON.parse(keypairId)))
   );
   const rpcDevUrl = "https://api.devnet.solana.com";
-  const rpcLocalUrl = "https://api.devnet.solana.com";
+  const rpcLocalUrl = "http://localhost:8899";
   // const rpcUrl = rpcDevUrl;
   const rpcUrl = rpcLocalUrl;
   const processed = "processed";
@@ -29,6 +30,37 @@ function initSynftProgram() {
     provider
   );
   return synftProgram;
+}
+
+export async function findChildrenMetaPda(parent_mint, child_mint, program) {
+  return await PublicKey.findProgramAddress(
+    [
+      Buffer.from(anchor.utils.bytes.utf8.encode("children-of")),
+      parent_mint.toBuffer(),
+      child_mint.toBuffer(),
+    ],
+    program.programId
+  );
+}
+
+export async function findParentMetaPda(mint, program) {
+  return await PublicKey.findProgramAddress(
+    [
+      Buffer.from(anchor.utils.bytes.utf8.encode("parent-metadata-seed")),
+      mint.toBuffer(),
+    ],
+    program.programId
+  );
+}
+
+export async function findCrankMetaPda(mint, program) {
+  return await PublicKey.findProgramAddress(
+    [
+      Buffer.from(anchor.utils.bytes.utf8.encode("crank-seed")),
+      mint.toBuffer(),
+    ],
+    program.programId
+  );
 }
 
 export const SynftProgram = initSynftProgram();
--- render_scripts/transfer_crank.ts
+++ render_scripts/transfer_crank.ts
@@ -1,8 +1,32 @@
-import {SynftProgram}  from "./common";
+import { PublicKey } from "@solana/web3.js";
+import { SynftProgram, findParentMetaPda, findChildrenMetaPda, findCrankMetaPda } from "./common";
 
-function main() {
-  console.log(SynftProgram.programId);
+async function main() {
+
+  let pdas = await fetchAllchildrenMetadataV2PDAs(SynftProgram);
+  pdas.forEach(element => {
+    if (element.account.isMutated) {
+      console.log(element.account.isMutated.toString());
+    }
+  })
+  // 1.有孩子的
+  //   a. 有两层， 需要执行init、process、end
+
+  // 2. 没有孩子的 执行init、end
+  //   a. root指向自己的，说明是前两层
+  //   b. root不指向自己，说明是后两层
 }
 
+async function fetchAllchildrenMetadataV2PDAs(program) {
+  // const filter: any = [];
+  // filter.push({
+  //   memcmp: {
+  //     offset: 22, //need to prepend 34 bytes for ChildrenMetadataV2 is_mutated
+  //     bytes: "r3ye1XJ",
+  //   },
+  // });
+  const pdas = await program.account.childrenMetadataV2.all();
+  return pdas;
+}
 main();
 
