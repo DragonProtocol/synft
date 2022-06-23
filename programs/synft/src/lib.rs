eb3066da6eb6e1215e4334ea57f1fb434683a0cc
lmf004 <liumf2005@126.com>
2022-06-23 13:12:32 +0800

    clean code

--- programs/synft/src/lib.rs
+++ programs/synft/src/lib.rs
@@ -10,45 +10,6 @@ declare_id!("nftNZSYP2LiWYW4zDdcNwimx6jWjJ8FnfN71o1ukd4p");
 pub mod synft {
     use super::*;
 
-    // pub fn initialize_inject(
-    //     ctx: Context<InitializeInject>,
-    //     reversible: bool,
-    //     bump: u8,
-    // ) -> Result<()> {
-    //     instructions::initialize_inject::handler(ctx, reversible, bump)
-    // }
-
-    // pub fn initialize_fungible_token_inject(
-    //     ctx: Context<InitializeFungibleTokenInject>,
-    //     reversible: bool,
-    //     bump: u8,
-    //     inject_fungible_token_amount: u64,
-    // ) -> Result<()> {
-    //     instructions::initialize_fungible_token_inject::handler(
-    //         ctx,
-    //         reversible,
-    //         bump,
-    //         inject_fungible_token_amount,
-    //     )
-    // }
-
-    // pub fn initialize_sol_inject(
-    //     ctx: Context<InitializeSolInject>,
-    //     reversible: bool,
-    //     bump: u8,
-    //     inject_sol_amount: u64,
-    // ) -> Result<()> {
-    //     instructions::initialize_sol_inject::handler(ctx, reversible, bump, inject_sol_amount)
-    // }
-
-    // pub fn extract(ctx: Context<Extract>, _bump: u8) -> Result<()> {
-    //     instructions::extract::handler(ctx, _bump)
-    // }
-
-    // pub fn extract_sol(ctx: Context<ExtractSol>, _bump: u8) -> Result<()> {
-    //     instructions::extract_sol::handler(ctx, _bump)
-    // }
-
     pub fn nft_copy(
         ctx: Context<NftCopy>,
         name: String,
@@ -57,14 +18,6 @@ pub mod synft {
     ) -> Result<()> {
         instructions::nft_copy::handler(ctx, name, symbol, uri)
     }
-
-    // pub fn burn_for_sol(ctx: Context<BurnForSol>) -> Result<()> {
-    //     instructions::burn_for_sol::handler(ctx)
-    // }
-
-    // pub fn burn_for_token(ctx: Context<BurnForToken>) -> Result<()> {
-    //     instructions::burn_for_token::handler(ctx)
-    // }
 
     pub fn inject_to_root_v2(
         ctx: Context<InjectToRootV2>,
